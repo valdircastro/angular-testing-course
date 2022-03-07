e9d72c1b5a1dedabefcd7d12b9daf5258c63a237
Valdir Arruda de C. Júnior <valdir_castro@virgo.atlantico.net.br>
2022-03-07 13:25:09 -0300

    Start project

---
+++ src/app/courses/services/calculator.service.spec.ts
@@ -0,0 +1,29 @@
+import {CalculatorService} from './calculator.service';
+import {LoggerService} from './logger.service';
+
+describe('CalculatorService', () => {
+
+  it('should add two numbers', () => {
+    const logger = jasmine.createSpyObj('LoggerService', ['log']);
+
+    // Returns a value from mock dependency method
+    // logger.log.and.returnValue('Value');
+
+    const calculator = new CalculatorService(logger);
+
+    const result = calculator.add(2, 2);
+
+    expect(result).toBe(4);
+
+    expect(logger.log).toHaveBeenCalledTimes(1);
+  });
+
+  it('should subtract two numbers', () => {
+    const calculator = new CalculatorService(new LoggerService());
+
+    const result = calculator.subtract(2, 2);
+
+    expect(result).toBe(0, 'unexpected subtract result');
+  });
+
+});
