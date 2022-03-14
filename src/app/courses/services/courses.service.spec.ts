5545b396153bf9649375a455cdb79d5e2556462c
Valdir Castro <valdir.a.c.junior@gmail.com>
2022-03-14 14:36:27 -0300

    add courses.service.spec.ts to repo

---
+++ src/app/courses/services/courses.service.spec.ts
@@ -0,0 +1,39 @@
+import {CoursesService} from './courses.service';
+import {TestBed} from '@angular/core/testing';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
+import * as http from 'http';
+import {COURSES} from '../../../../server/db-data';
+
+describe('CoursesService', () => {
+
+  let coursesService: CoursesService;
+  let httpTestingController: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ],
+      providers: [
+        CoursesService
+      ]
+    });
+
+    coursesService = TestBed.inject(CoursesService);
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
+
+  it('should retrieve all courses', () => {
+    coursesService.findAllCourses().subscribe(courses => {
+      expect(courses).toBeTruthy('No courses returned');
+      expect(courses.length).toBe(12, 'Incorrect number of courses');
+
+      const course = courses.find(c => c.id === 12);
+      expect(course.titles.description).toBe('Angular Testing Course');
+    });
+
+    const req = httpTestingController.expectOne('/api/courses');
+    expect(req.request.method).toEqual('GET');
+    req.flush({payload: Object.values(COURSES)});
+  });
+});
