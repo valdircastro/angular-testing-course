203d56406e6ceb98ac86a1ac0f46f765985a2992
Valdir Castro <valdir.a.c.junior@gmail.com>
2022-03-29 00:07:45 -0300

    add params check test

--- src/app/courses/services/courses.service.spec.ts
+++ src/app/courses/services/courses.service.spec.ts
@@ -2,7 +2,9 @@ import {CoursesService} from './courses.service';
 import {TestBed} from '@angular/core/testing';
 import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
 import * as http from 'http';
-import {COURSES} from '../../../../server/db-data';
+import {COURSES, findLessonsForCourse} from '../../../../server/db-data';
+import {Course} from '../model/course';
+import {HttpErrorResponse} from '@angular/common/http';
 
 describe('CoursesService', () => {
 
@@ -46,6 +48,56 @@ describe('CoursesService', () => {
     const req = httpTestingController.expectOne('/api/courses/12');
     expect(req.request.method).toEqual('GET');
     req.flush(COURSES[12]);
+  });
+
+  it('should save the course data', () => {
+
+    const changes: Partial<Course> = {titles: {description: 'Testing Course'}};
+
+    coursesService.saveCourse(12, changes).subscribe(course => {
+      expect(course.id).toBe(12);
+    });
+
+    const req = httpTestingController.expectOne('/api/courses/12');
+    expect(req.request.method).toEqual('PUT');
+    expect(req.request.body.titles.description).toEqual(changes.titles.description);
+    req.flush({
+      ...COURSES[12],
+      ...changes
+    });
+  });
+
+  it('should give a error if save course fails', () => {
+    const changes: Partial<Course> = {titles: {description: 'Testing Course'}};
+
+    coursesService.saveCourse(12, changes).subscribe(
+      () => {
+        fail('the save course operation should have failed');
+      },
+      (error: HttpErrorResponse) => {
+        expect(error.status).toBe(500);
+      });
+
+    const req = httpTestingController.expectOne('/api/courses/12');
+    expect(req.request.method).toEqual('PUT');
+    req.flush('Save course failed', {status: 500, statusText: 'Internal Server Error'});
+  });
+
+  it('should find a list of lessons', () => {
+    coursesService.findLessons(12).subscribe(lessons => {
+      expect(lessons).toBeTruthy();
+      expect(lessons.length).toBe(3);
+    });
+
+    const req = httpTestingController.expectOne(r => r.url === '/api/lessons');
+    expect(req.request.method).toEqual('GET');
+    expect(req.request.params.get('courseId')).toEqual('12');
+    expect(req.request.params.get('filter')).toEqual('');
+    expect(req.request.params.get('sortOrder')).toEqual('asc');
+    expect(req.request.params.get('pageNumber')).toEqual('0');
+    expect(req.request.params.get('pageSize')).toEqual('3');
+
+    req.flush({payload: findLessonsForCourse(12).slice(0, 3)});
   });
 
   afterEach(() => {
