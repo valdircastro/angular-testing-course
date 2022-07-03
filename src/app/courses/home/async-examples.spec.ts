25df212ca8a146d6fed946ff9d8d2771f46f4fc1
Valdir Castro <valdir.a.c.junior@gmail.com>
2022-07-03 01:23:52 -0300

    fakeAsync with observables (32)

--- src/app/courses/home/async-examples.spec.ts
+++ src/app/courses/home/async-examples.spec.ts
@@ -1,4 +1,6 @@
-import {fakeAsync, flush, tick} from '@angular/core/testing';
+import {fakeAsync, flush, flushMicrotasks, tick} from '@angular/core/testing';
+import {of} from 'rxjs';
+import {delay} from 'rxjs/operators';
 
 fdescribe('Async Testing Examples', () => {
   it('Asynchronous test examples with Jasmine done()', (done) => {
@@ -24,20 +26,51 @@ fdescribe('Async Testing Examples', () => {
     })
   );
 
-  fit('Asynchronous test example - plain Promise()', () => {
+  it('Asynchronous test example - plain Promise()', fakeAsync(() => {
     let test = false;
 
     console.log('Creating a promise');
 
     Promise.resolve().then(() => {
       console.log('Promise first then() evaluated successfully');
+      test = true;
       Promise.resolve().then(() => {
         console.log('Promise second then() evaluated successfully');
-        test = true;
       });
     });
 
+    flushMicrotasks();
+
     console.log('Running test assertions');
     expect(test).toBeTruthy();
-  });
+  }));
+
+  it('Asynchronous test example - Promises + set timeout()', fakeAsync(() => {
+    let counter = 0;
+
+    Promise.resolve().then(() => {
+      counter += 10;
+      setTimeout(() => {
+        counter += 1;
+      }, 1000);
+    });
+
+    expect(counter).toBe(0);
+    flushMicrotasks();
+    expect(counter).toBe(10);
+    flush();
+    expect(counter).toBe(11);
+  }));
+
+  it('Asynchronous test examples - Observables()', fakeAsync(() => {
+    let test = false;
+    console.log('Creating Observables');
+    const test$ = of(test).pipe(delay(1000));
+    test$.subscribe(() => {
+      test = true;
+    });
+    tick(1000);
+    console.log('Running test assertions');
+    expect(test).toBe(true);
+  }));
 });
